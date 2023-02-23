166c76c6693192a5744d0189af02d196c5ffcd2c
grigoriliev <grigor@grigoriliev.com>
2023-02-23 14:34:31 +0200

    jlscp: update copyright notice in source files

--- src/main/java/org/linuxsampler/lscp/event/EffectInstanceInfoListener.java
+++ src/main/java/org/linuxsampler/lscp/event/EffectInstanceInfoListener.java
@@ -1,23 +1,22 @@
 /*
  *   jlscp - a java LinuxSampler control protocol API
  *
- *   Copyright (C) 2011 Grigor Iliev <grigor@grigoriliev.com>
+ *   Copyright (C) 2005-2023 Grigor Iliev <grigor@grigoriliev.com>
  *
  *   This file is part of jlscp.
  *
- *   jlscp is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License version 2
- *   as published by the Free Software Foundation.
+ *   jlscp is free software: you can redistribute it and/or modify it under
+ *   the terms of the GNU General Public License as published by the Free
+ *   Software Foundation, either version 3 of the License, or (at your option)
+ *   any later version.
  *
- *   jlscp is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *   GNU General Public License for more details.
+ *   jlscp is distributed in the hope that it will be useful, but WITHOUT
+ *   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *   FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ *   more details.
  *
- *   You should have received a copy of the GNU General Public License
- *   along with jlscp; if not, write to the Free Software
- *   Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *   MA  02111-1307  USA
+ *   You should have received a copy of the GNU General Public License along
+ *   with jlscp. If not, see <https://www.gnu.org/licenses/>. 
  */
 package org.linuxsampler.lscp.event;
 
